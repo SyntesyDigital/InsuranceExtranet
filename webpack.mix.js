570c7ffe18748e2e2983400cbc0c5de4cc1a1c5b
ninidc <ndelcast.dev@gmail.com>
2019-09-23 18:49:05 +0200

    - Configure Copy Webpack Plugin for copying all Js assets

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,11 +1,46 @@
 const mix = require('laravel-mix');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
 
 require('laravel-mix-merge-manifest');
-
 mix.setPublicPath('../../public').mergeManifest();
 
-mix.react('Resources/assets/js/app.js', 'modules/extranet/js')
+
+// ---------------------------------------- //
+//      COPIE LIBS
+// ---------------------------------------- //
+mix.webpackConfig({
+    plugins: [
+        new CopyWebpackPlugin([
+            {
+                from: 'Resources/assets/js/admin/',
+                to: '../public/modules/extranet/js/admin/',
+                toType: 'dir'
+            },
+            {
+                from: 'Resources/assets/js/libs/',
+                to: '../public/modules/extranet/js/libs/',
+                toType: 'dir'
+            },
+            {
+                from: 'Resources/assets/plugins/',
+                to: '../public/modules/extranet/plugins/',
+                toType: 'dir'
+            }
+        ])
+    ]
+});
+// ---------------------------------------- //
+
+
+// ---------------------------------------- //
+//      COMPILE ASSETS
+// ---------------------------------------- //
+mix
+    .react('Resources/assets/js/app.js', 'modules/extranet/js')
     .sass('Resources/assets/sass/app.scss', 'modules/extranet/css');
+// ---------------------------------------- //
+
 
 if (mix.inProduction()) {
     mix.version();
