993ecc4cb4d5c31914a47f659df2e372c7e8f8fb
Nicolas DELCASTILLO <nicolas.delcastillo@syntesy.io>
2019-12-11 16:40:40 +0100

    FIX errors

--- Repositories/ElementRepository.php
+++ Repositories/ElementRepository.php
@@ -427,13 +427,13 @@ class ElementRepository extends BaseRepository
     }
 
     public function getErrors()
-    {    
+    {
       return [
         'columns' => [
           'icon' => 'Message',
           'name' => 'Element'
         ],
-        'rows' => Error::whereIn('errorable_type', ElementField::class)
+        'rows' => Error::where('errorable_type', ElementField::class)
           ->get()
           ->map(function($error) {
             return [
