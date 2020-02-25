2cde5587269d6003cad0bd44e86ca1072e2c292b
Nicolas DELCASTILLO <nicolas.delcastillo@syntesy.io>
2020-02-25 16:35:52 +0100

    FIX RolePermissions migration

--- Services/RolesPermissions/Migrations/2020_01_21_175817_roles_permissions_users.php
+++ Services/RolesPermissions/Migrations/2020_01_21_175817_roles_permissions_users.php
@@ -12,7 +12,13 @@ class RolesPermissionsUsers extends Migration
     public function up()
     {
         DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        Schema::dropIfExists('users_roles');
+        Schema::dropIfExists('users_permissions');
+        Schema::dropIfExists('roles_permissions');
         Schema::dropIfExists('users');
+        Schema::dropIfExists('roles');
+        Schema::dropIfExists('permissions');
+        Schema::dropIfExists('permissions_groups');
         DB::statement('SET FOREIGN_KEY_CHECKS=1;');
 
         Schema::create('users', function (Blueprint $table) {
