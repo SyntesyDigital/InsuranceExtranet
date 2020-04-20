08bb2199bbabf36c31c6d124c215517ec8c8aa24
Sergi Mínguez Regidor <sergi.minguez@cocreativa.com>
2020-04-20 20:11:10 +0200

    new selector

---
+++ Resources/views/front/partials/session_select.blade.php
@@ -0,0 +1,68 @@
+
+
+@if(Auth::user()->supervue)
+
+	@php
+		$sessions = Auth::user()->sessions;
+		$options = [];
+		foreach($sessions as $session) {
+			$options[$session->session] = $session->lib;
+		}
+		$current = Auth::user()->session_info->{'USEREXT.id_per'};
+	@endphp
+
+	{!!
+		Form::select(
+			'sessions',
+			$options,
+			$current,
+			[
+				'class' => 'form-control session-changer',
+				'id' => 'session-changer'
+			]
+		)
+	!!}
+@else
+	Bonjour, {{Auth::user()->firstname}}
+@endif
+
+
+@push('javascripts')
+<script>
+
+	$(function(){
+
+		if($('#session-changer').length > 0){
+			$('#session-changer').change(function(e) {
+
+				var sessionId = e.target.value;
+
+				$('#session-changer').prop( "disabled", true );
+
+				//post sessions
+				$.ajax({
+					method: "POST",
+					url: '{{ route('session.update') }}',
+					data: {
+								session_id : sessionId,
+								_token: $('meta[name="csrf-token"]').attr('content')
+							},
+					dataType: 'json'
+				}).done(function(response) {
+
+							//console.log(response);
+							//window.location.href = response.redirect;
+							window.location.href = '/';
+
+				}).fail(function(jqXHR, textStatus) {
+						//el.find('.modal-footer .message').html(jqXHR.responseJSON.message);
+							window.location.href = '/';
+				});
+
+
+			});
+		}
+	});
+</script>
+
+@endpush
