90b1cdcbba3805e7b5ec252e5a5630c26e64273a
Nicolas DELCASTILLO <nicolas.delcastillo@syntesy.io>
2020-04-29 19:01:57 +0200

    - Export download JSON file

--- Resources/assets/js/back/api/client.js
+++ Resources/assets/js/back/api/client.js
@@ -1,30 +1,30 @@
 import ApolloClient, { gql } from 'apollo-boost';
 
 const client = new ApolloClient({
-  uri: ASSETS + '/graphql',
-  request: operation => {
-    operation.setContext({
-      headers: {
-        authorization: 'Bearer ' + SESSION.token,
-      },
-    });
-  },
+    uri: ASSETS + '/graphql',
+    request: operation => {
+        operation.setContext({
+            headers: {
+                authorization: 'Bearer ' + SESSION.token,
+            },
+        });
+    },
 });
 
 export function mutation(mutation,variables) {
-  return client
-    .mutate({
-      mutation: mutation,
-      variables: variables,
-    })
+    return client
+        .mutate({
+            mutation: mutation,
+            variables: variables,
+        });
 }
 
 export function query(query,variables) {
-  return client
-    .query({
-      query: query,
-      variables: variables,
-    })
+    return client
+        .query({
+            query: query,
+            variables: variables,
+        });
 }
 
 
