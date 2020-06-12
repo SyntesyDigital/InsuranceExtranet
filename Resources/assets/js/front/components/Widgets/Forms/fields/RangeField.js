dab2cb2c1c87c1ab9dedac3fcb0b93b4d79f83b2
Francisco Barceló <franciscobarcelo@Mac-mini-de-Francisco.local>
2020-06-12 18:04:58 +0200

    - Fix label z-index on current value RangeField component

--- Resources/assets/js/front/components/Widgets/Forms/fields/RangeField.js
+++ Resources/assets/js/front/components/Widgets/Forms/fields/RangeField.js
@@ -6,22 +6,42 @@ import 'react-rangeslider/lib/index.css'
 export default class RangeField extends Component {
     constructor(props, context) {
         super(props, context)
-        
+
+        this.state = {
+            active: '',
+            range: null
+        }
     }
 
     handleChange(value) {
         this.props.onChange(this.props.name, value);
+        this.setState({
+            range: value
+        })
+        if(this.state.range <= (this.props.minValue + 20 )) { 
+            this.setState({
+                active: 'hidden-label-right'
+            });
+        }else if ((this.state.range >= (this.props.maxValue - 20 ))){
+            this.setState({
+                active: 'hidden-label-left'
+            });
+        }else {
+            this.setState({
+                active: ''
+            })
+       }
     }
 
     render() {
         const rangeLabels = {
-            0 : this.props.minValue + '€',
-            1000 : this.props.maxValue + '€'
+            0: this.props.minValue + '€',
+            1000: this.props.maxValue + '€'
         }
 
         return (
             <div className="range-field">
-                <div className="row container-range">
+                <div className={`row container-range ${this.state.active}`}>
                     <div className="col-md-2 container-label">
                         <p>{this.props.label}</p>
                     </div>
