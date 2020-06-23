c4e312d11a4ffd2e2d4d91b8e427fb5c9fc1a618
Francisco Barceló <franciscobarcelo@Mac-mini-de-Francisco.local>
2020-06-23 17:22:06 +0200

    - Fixes on Chat component [SOLVED]

--- Resources/assets/js/front/components/Widgets/Lists/Chat/Chat.js
+++ Resources/assets/js/front/components/Widgets/Lists/Chat/Chat.js
@@ -1,133 +1,81 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import ListParser from '../ListParser';
-
 import {
-  parseNumber,
   parseDate,
-  getConditionalFormating,
-  hasConditionalFormatting
 } from '../../functions';
 
 export default class Chat extends Component {
 
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    hasConditionalFormatting(conditionalFormatting) {
-      if(conditionalFormatting.color !== undefined){
-        return true;
-      }
-      return false;
-    }
+  renderItem(item, elementObject) {
 
-    renderField(item,identifier,field) {
+    var infos = [];
+    var email = item['expediteur'];
+    var date = parseDate(item['DTCRE_MAIL'], elementObject);
+    var content = item['CORPS'];
 
-      var value = item[identifier];
-      var email = new RegExp(/[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,15}/g).test(value)
+    infos.push(
+      <div className={'field-container'}>
+        {date ? <div className="container-date"><div className="has-date">{date}</div></div> : null}
+        {email ? <div className="container-mail"><div className="has-mail">{email}</div></div> : null}
+        {content ? <div className="container-content" dangerouslySetInnerHTML={{__html: content}} /> : null}
+      </div>
+    );
 
-      if(field.type == "date") {
-          value = parseDate(value,field);
-          return <div className={'has-date'} dangerouslySetInnerHTML={{__html: value}} />
-      }
-      else if(field.type == "number") {
-          value = parseNumber(value,field);
-      }
-      else if(email == true) {
-          return <div className={'has-mail'} dangerouslySetInnerHTML={{__html: value}} />
-      } 
-      
-      var style = getConditionalFormating(field,value);
-      var hasColor = hasConditionalFormatting(style);
-      
-      if(field.type == "file"){
-        return <div dangerouslySetInnerHTML={{__html: value}} />
-      }
-      else if(field.settings.hasRoute !== undefined && field.settings.hasRoute != null){
-
-        return <div dangerouslySetInnerHTML={{__html: item[identifier+"_url"]}} />
-      }
-      else {
-        return <div className={hasColor ? 'has-color' : ''} style={style} dangerouslySetInnerHTML={{__html: value}} />
-      }
-
-    }
-
-    renderItem(item,elementObject) {
-
-      var file = null;
-      var infos = [];
-      
-      for(var key in elementObject.fields){
-        var identifier =  elementObject.fields[key].identifier
-        if(elementObject.fields[key].type == 'file'){
-          file = this.renderField(item,identifier,elementObject.fields[key]);
-        } 
-        else { 
-          infos.push(
-              <div className="field-container" key={key}>
-                  {this.renderField(item,identifier,elementObject.fields[key])}
-              </div>
-          );
-        }
-        console.log("infos :: ", infos);
-      }
-
-      return (
-          <div>
-            <div className={"file-infos-container "+(file == null ? 'no-document' : '')}>
-              {file != null && 
-                <div className="file-icon">
-                  {file}
-                </div>
-              }
-              <div className="file-infos">
-                {infos}
-              </div>
-            </div>
+    return (
+      <div className={email ? 'container-active' : 'container-inactive'}>
+        <div className={"file-infos-container"}>
+          <div className="file-infos">
+            {infos}
           </div>
-        );
-    }
+        </div>
+      </div>
+    );
+  }
 
-    render() {
-        return (
-            <div>
-              <ListParser
-                customClass="table-chat-container"
-                field={this.props.field}
-                elementObject={this.props.elementObject}
-                model={this.props.model}
-                pagination={this.props.pagination}
-                itemsPerPage={this.props.itemsPerPage}
-                columns={this.props.columns}
-                parameters={this.props.parameters}
-                renderItem={this.renderItem.bind(this)}
-                identifier={'chat-field'}
-              />
-            </div>
+  render() {
+    return (
+      <div>
+        <ListParser
+          customClass="table-chat-container"
+          field={this.props.field}
+          elementObject={this.props.elementObject}
+          model={this.props.model}
+          pagination={this.props.pagination}
+          itemsPerPage={this.props.itemsPerPage}
+          columns={this.props.columns}
+          parameters={this.props.parameters}
+          renderItem={this.renderItem.bind(this)}
+          identifier={'chat-field'}
+          onReverse={true}
+        />
+      </div>
 
-        );
-    }
+    );
+  }
 }
 
 if (document.getElementById('chat-list')) {
 
-   document.querySelectorAll('[id=chat-list]').forEach(function(element){
-       var field = element.getAttribute('field');
-       var elementObject = element.getAttribute('elementObject');
-       var model = element.getAttribute('model');
-       var itemsPerPage = element.getAttribute('itemsPerPage');
-       var parameters = element.getAttribute('parameters');
-       var columns = element.getAttribute('columns');
+  document.querySelectorAll('[id=chat-list]').forEach(function (element) {
+    var field = element.getAttribute('field');
+    var elementObject = element.getAttribute('elementObject');
+    var model = element.getAttribute('model');
+    var itemsPerPage = element.getAttribute('itemsPerPage');
+    var parameters = element.getAttribute('parameters');
+    var columns = element.getAttribute('columns');
 
-       ReactDOM.render(<Chat
-           field={field}
-           elementObject={elementObject}
-           model={model}
-           itemsPerPage={itemsPerPage}
-           columns={columns}
-           parameters={parameters}
-         />, element);
-   });
+    ReactDOM.render(<Chat
+      field={field}
+      elementObject={elementObject}
+      model={model}
+      itemsPerPage={itemsPerPage}
+      columns={columns}
+      parameters={parameters}
+    />, element);
+  });
 }
--- Resources/views/front/partials/styles/chat.blade.php
+++ Resources/views/front/partials/styles/chat.blade.php
@@ -1,35 +1,35 @@
 <style type="text/css">
 
     /* sender */
-    body .chat-field.container-active{
+    body .chat-field .container-active{
         background-color: {{$senderBackgroundColorChat}};
         border: 1.5px solid {{$senderBorderColorChat}};
         border-radius: {{$senderBorderRadiusChat}}
     }
-    body .chat-field.container-active .field-container{
+    body .chat-field .container-active .field-container{
         color: {{$senderColorChat}};
         font-size: {{$senderFontSizeChat}};
     }
-    body .chat-field.container-active .field-container .has-date{
+    body .chat-field .container-active .field-container .has-date{
         color: {{$senderDateColorChat}};
         font-size: {{$senderDateFontSizeChat}};
     }
-    body .chat-field.container-active .field-container .has-mail{
+    body .chat-field .container-active .field-container .has-mail{
         color: {{$senderMailColorChat}};
         font-size: {{$senderMailFontSizeChat}};
     }
 
     /* recipient */
-    body .chat-field:not(.container-active){
+    body .chat-field .container-inactive{
         background-color: {{$recipientBackgroundColorChat}};
         border: 1px solid {{$recipientBorderColorChat}};
         border-radius: {{$recipientBorderRadiusChat}};
     }
-    body .chat-field:not(.container-active) .field-container{
+    body .chat-field .container-inactive .field-container{
         color: {{$recipientColorChat}};
         font-size: {{$recipientFontSizeChat}};
     }
-    body .chat-field:not(.container-active) .field-container .has-date{
+    body .chat-field .container-inactive .field-container .has-date{
         color: {{$recipientDateColorChat}};
         font-size: {{$recipientDateFontSizeChat}};
     }
@@ -47,16 +47,7 @@
         color: {{$buttonColorHoverChat}};
         border: 1px solid {{$buttonborderHoverColorChat}};
     }
-    @if (isset($alignButtonChat) && $alignButtonChat == 'top')
-        body .table-chat-container .documents-container{
-            order: 2;
-        }
-    @endif   
-    @if (isset($alignButtonChat) && $alignButtonChat == 'bottom')
-        body .table-chat-container .documents-container{
-            order: unset;
-        }
-    @endif 
+
 </style>
 
 
