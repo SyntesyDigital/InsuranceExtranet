78736ce08b1008d2bb704e240c867cac03124bce
Sergi Mínguez Regidor <sergi.minguez@cocreativa.com>
2020-07-02 14:00:41 +0200

    update footer

--- Resources/views/front/partials/footer.blade.php
+++ Resources/views/front/partials/footer.blade.php
@@ -22,18 +22,3 @@
 		<!-- end Col 2 -->
 	</footer><!-- END FOOTER -->
 @endif
-
-<<<<<<< HEAD
-		&nbsp;&nbsp;&nbsp;
-		<div class="version" style="">
-<<<<<<< HEAD
-			Architect v1.18.0@dev v1.18.7@dev
-=======
-			Architect v1.17.0 v1.17.5
->>>>>>> master
-		</div>
-  </div>
-	<!-- end Col 2 -->
-</footer><!-- END FOOTER -->
-=======
->>>>>>> d53eb060ef47ada4613a5b4df924733850c0d411
