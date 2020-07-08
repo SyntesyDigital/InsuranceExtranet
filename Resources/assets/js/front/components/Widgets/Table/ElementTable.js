c19dd3fe7123e73c490fbc345d7ed06c63597787
Sergi Mínguez Regidor <sergi.minguez@cocreativa.com>
2020-07-08 14:07:29 +0200

    update form

--- Resources/assets/js/front/components/Widgets/Table/ElementTable.js
+++ Resources/assets/js/front/components/Widgets/Table/ElementTable.js
@@ -33,7 +33,7 @@ export default class ElementTable extends Component {
         redirectUrl : redirectUrl
       });
 
-
+      
     }
 
     handleModalClose(){
@@ -56,7 +56,6 @@ export default class ElementTable extends Component {
             <div>
 
               <ModalTable
-                field={this.props.field}
                 display={this.state.displayModal}
                 id={"modal-table-component-"+this.state.id}
                 zIndex={1000}
