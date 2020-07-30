3b624a0b533498b9b64c174d44d5f387eb582df2
Nicolas DELCASTILLO <nicolas.delcastillo@syntesy.io>
2020-07-30 20:10:51 +0200

    FIX menu footer

--- Resources/views/front/partials/menu_footer.blade.php
+++ Resources/views/front/partials/menu_footer.blade.php
@@ -1,35 +1,36 @@
 @if(isset($menu))
 	<ul class="container-menu">
-		@php $footerItems = 0; @endphp
-		@foreach($menu as $index => $menuElement)
-			<li class="menu-item">
-				@php
-					$link = format_link($menuElement);
-					$hasChildren = sizeof($menuElement["children"]) > 0 ? 1 : 0;
-				@endphp
-				@if(isset($link))
-					@if($footerItems > 0)
-						<span class="footer-separator"> | </span>
-					@endif
-					<a href="{{$link["url"]}}" id="{{$link["id"]}}" class="{{$link["class"]}}" >
+        @php $footerItems = 0; @endphp
+        
+        @foreach($menu as $index => $menuElement)
+            @if(isset($menuElement["name"]))
+                <li class="menu-item">
+                    @php
+                        $link = format_link($menuElement);
+                        $hasChildren = sizeof($menuElement["children"]) > 0 ? 1 : 0;
+                    @endphp
+                    @if(isset($link))
+                        @if($footerItems > 0)
+                            <span class="footer-separator"> | </span>
+                        @endif
+                        <a href="{{$link["url"]}}" id="{{$link["id"]}}" class="{{$link["class"]}}" >
 
-						@if(isset($link["icon"]))
-							<i class="{{$link['icon']}}"></i>
-						@endif
-						{{$link["name"]}}
-					</a>
-				@else
-					<p>
-						@if($footerItems > 0)
-							<span class="footer-separator"> | </span>
-						@endif
-						{{ $menuElement["name"] or '' }}
-					</p>
-				@endif
-
-			</li>
-			@php $footerItems++; @endphp
-
+                            @if(isset($link["icon"]))
+                                <i class="{{$link['icon']}}"></i>
+                            @endif
+                            {{$link["name"]}}
+                        </a>
+                    @else
+                        <p>
+                            @if($footerItems > 0)
+                                <span class="footer-separator"> | </span>
+                            @endif
+                            {{ $menuElement["name"] or '' }}
+                        </p>
+                    @endif
+                </li>
+			    @php $footerItems++; @endphp
+            @endif
 		@endforeach
 	</ul>
 @endif
