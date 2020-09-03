b4f59037c549f6a5d360932a2d6c62c01e385765
Dani <daniel@syntesy.io>
2020-09-03 12:28:17 +0200

    merge and fixes on site configurations

--- Services/SiteConfigurations/Helpers/get_site_configuration.php
+++ Services/SiteConfigurations/Helpers/get_site_configuration.php
@@ -3,17 +3,17 @@
 if (!function_exists('get_site_configuration')) {
     function get_site_configuration($identifier, $group = 'general')
     {
-        $storedSiteConfigurations = \Cache::get($group.'siteConfigurations');
+        $storedSiteConfigurations = \Cache::get($group.'SiteConfigurations');
 
         if (!$storedSiteConfigurations) {
             $seconds = 24 * 3600;
             $siteConfiguration = \Modules\Extranet\Services\SiteConfigurations\Entities\SiteConfiguration::where('identifier', $group)->first();
             $storedSiteConfigurations = (new \Modules\Extranet\Services\SiteConfigurations\Transformers\SiteConfigurationsFormTransformer($siteConfiguration))->toArray();
-            \Cache::put($group.'siteConfigurations', $storedSiteConfigurations, $seconds);
+            \Cache::put($group.'SiteConfigurations', $storedSiteConfigurations, $seconds);
         }
 
         return isset($storedSiteConfigurations[$identifier])
             ? $storedSiteConfigurations[$identifier]->value
-            : config('siteConfigurations.default')[$identifier];
+            : config('SiteConfigurations.default')[$identifier];
     }
 }
