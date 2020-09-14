2293c6c46bae09270e3673f8473ea878cf5085a7
Francisco Barceló <franciscobarcelo@Mac-mini-de-Francisco.local>
2020-09-14 19:02:58 +0200

    - Fix background transparent field status

--- Resources/assets/js/front/components/Widgets/ElementCard/fields/DefaultField.js
+++ Resources/assets/js/front/components/Widgets/ElementCard/fields/DefaultField.js
@@ -5,67 +5,74 @@ import { Row, Col } from 'react-bootstrap';
 
 export default class DefaultField extends Component {
 
-  constructor(props) {
-    super(props);
-  }
+    constructor(props) {
+        super(props);
+    }
 
-  render() {
-    const { label, value, valueColor, valueBackgroundColor, stripped, labelAlign, valueAlign, inline } = this.props;
+    render() {
+        const { label, value, valueColor, valueBackgroundColor, stripped, labelAlign, valueAlign, inline } = this.props;
+        console.log("valueBackgroundColor :: ", (valueBackgroundColor + ' !important'))
+        const divStyle = {
+            overflow: 'hidden',
+        }
+        const divLabel = {
+            textAlign: labelAlign
+        }
+        const divValue = {
+            textAlign: valueAlign
+        }
+        const labelStyle = {
+            padding: '8px',
+            marginBottom: '0'
+        };
 
-    const divStyle = {
-      overflow: 'hidden',
+        const spanStyles = {
+            backgroundColor: valueBackgroundColor,
+            textAlign: labelAlign,
+            color: valueColor,
+            borderRadius: '5px',
+            padding: '8px',
+            marginBottom: '0',
+            maxWidth: '100%',
+            display: 'inline-block',
+        };
+
+        return (
+            <div style={divStyle} className={(stripped ? 'stripped' : null)}>
+                <Row>
+                    <Col md={!inline ? 12 : 6} style={divLabel}>
+                        <label style={labelStyle}
+                            dangerouslySetInnerHTML={{ __html: label }}
+                        ></label>
+                    </Col>
+                    <Col md={!inline ? 12 : 6} style={divValue}>
+                        {this.props.icon ? <i className={this.props.icon}></i> : null}
+                        <span
+                            style={spanStyles}
+                            ref={(node) => {
+                                if (node) {
+                                    node.style.setProperty("background-color", valueBackgroundColor, "important");
+                                }
+                            }}
+                            className={valueAlign == 'center' ? 'value-center' : null}
+                            dangerouslySetInnerHTML={{ __html: value }}
+                        >
+                        </span>
+                    </Col>
+                </Row>
+            </div>
+        );
     }
-    const divLabel = {
-      textAlign: labelAlign
-    }
-    const divValue = {
-      textAlign: valueAlign
-    }
-    const labelStyle = {
-      padding: '8px',
-      marginBottom: '0'
-    };
-    const spanStyles = {
-      backgroundColor: valueBackgroundColor,
-      textAlign: labelAlign,
-      color: valueColor,
-      borderRadius: '5px',
-      padding: '8px',
-      marginBottom: '0',
-      maxWidth: '100%',
-      display: 'inline-block',
-    };
-console.log("this.props.icon :: " , this.props.icon)
-    return (
-      <div style={divStyle} className={(stripped ? 'stripped' : null)}>
-        <Row>
-          <Col md={!inline ? 12 : 6} style={divLabel}>
-            <label style={labelStyle}
-              dangerouslySetInnerHTML={{__html: label}}
-            ></label>
-          </Col>
-          <Col md={!inline ? 12 : 6} style={divValue}>
-            {this.props.icon ? <i className={this.props.icon}></i> : null}
-            <span 
-              style={spanStyles} className={valueAlign == 'center' ? 'value-center' : null}
-              dangerouslySetInnerHTML={{__html: value}}
-              >
-            </span>
-          </Col>
-        </Row>
-      </div>
-    );
-  }
 }
 
 DefaultField.propTypes = {
-  label: PropTypes.string.isRequired,
-  value: PropTypes.string,
-  valueColor: PropTypes.string,
-  valueBackgroundColor: PropTypes.string,
-  stripped: PropTypes.bool,
-  labelAlign: PropTypes.string,
-  valueAlign: PropTypes.string,
-  inline: PropTypes.bool,
-  icon: PropTypes.string
+    label: PropTypes.string.isRequired,
+    value: PropTypes.string,
+    valueColor: PropTypes.string,
+    valueBackgroundColor: PropTypes.string,
+    stripped: PropTypes.bool,
+    labelAlign: PropTypes.string,
+    valueAlign: PropTypes.string,
+    inline: PropTypes.bool,
+    icon: PropTypes.string
 };
