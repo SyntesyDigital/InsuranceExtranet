a3ba9b7f46a1194158921f50d4ba10b34e52024e
Sergi Mínguez Regidor <sergi.minguez@cocreativa.com>
2020-09-22 17:51:55 +0200

    update json

--- Resources/assets/js/front/components/Widgets/Forms/functions/fields.js
+++ Resources/assets/js/front/components/Widgets/Forms/functions/fields.js
@@ -21,7 +21,8 @@ import {
 } from './../constants';
 
 import {
-  checkIfExistJsonPath
+  checkIfExistJsonPath,
+  addKeyToJson
 } from './jsonpath.js';
 
 let jp = require('jsonpath');
@@ -598,67 +599,6 @@ function initJSONResult(jsonResult,procedure, isRootArray) {
 }
 
 
-/**
-*   Iterate recursively to make the json result.
-*/
-export function createJsonKey(paramArray,index,jsonResult) {
-
-  //console.log("setupJsonResult :: setup => ",paramArray,jsonResult,index,name,arrayPosition);
-
-  var name = paramArray[index];
-
-  /*
-  console.log("createJsonKey :: iteration : (paramArray,index,jsonResult,name,paramArray.length)",
-      paramArray,
-      index,
-      JSON.stringify(jsonResult),
-      name,
-      paramArray.length
-  );
-  */
-
-  if(jsonResult === undefined || jsonResult == null || $.isEmptyObject(jsonResult)){
-      jsonResult = {};
-  }
-
-  //define the json with null value
-  if(jsonResult[name] === undefined)
-      jsonResult[name] = null;
-
-  if(paramArray.length-1 > index ){
-      //continue with the next step
-      jsonResult[name] = createJsonKey(
-          paramArray,
-          index+1,
-          jsonResult[name]
-      );
-  }
-
-  return jsonResult;
-}
-
-/**
- * Function add a key that doesn't exist to JSON.
- * 
- * @param {*} json 
- * @param {*} jsonpath 
- */
-export function addKeyToJson(json,jsonpath){
-
-  if(jsonpath.indexOf('[') != -1){
-      console.error("addKeyToJSON : not possible to add key to array (jsonpath)",jsonpath);
-      return json;
-  }
-
-  //remove identifier from jsonpath
-  var jsonPathArray = jsonpath.split('.');
-
-  json = createJsonKey(jsonPathArray,1,json);
-
-  //console.log("createJsonKey :: final result (json)",json,'\n\n');
-
-  return json;
-}
 
 export function updateJSONWithFields(root,fields,json,values,formParameters) {
 
