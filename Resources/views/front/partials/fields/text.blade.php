c66d897f5b05778de0aeb32a81474d144fd4b13a
Robert Cardenas <robert.cardenas@damisis.com>
2020-11-27 17:07:02 +0100

    Add conditional visibility on fields

--- Resources/views/front/partials/fields/text.blade.php
+++ Resources/views/front/partials/fields/text.blade.php
@@ -1,7 +1,13 @@
-@if(!isset($div))
-<div id="{{$field['settings']['htmlId'] or ''}}" class="{{$field['settings']['htmlClass'] or ''}}">
-@endif
-  {{$field['value'][App::getLocale()] or ''}}
-@if(!isset($div))
-</div>
+@php
+    $visible = check_visible($field['settings'],$parameters);
+@endphp
+
+@if($visible)
+    @if(!isset($div))
+    <div id="{{$field['settings']['htmlId'] or ''}}" class="{{$field['settings']['htmlClass'] or ''}}">
+    @endif
+      {{$field['value'][App::getLocale()] or ''}}
+    @if(!isset($div))
+    </div>
+    @endif
 @endif
--- Resources/views/front/partials/fields/video.blade.php
+++ Resources/views/front/partials/fields/video.blade.php
@@ -2,11 +2,15 @@
   $link = isset($field['value']['url'][App::getLocale()]) ? $field['value']['url'][App::getLocale()] : '';
   $youtube_id = explode('/',$link);
   $youtube_id = $youtube_id[sizeof($youtube_id)-1];
+  $visible = check_visible($field['settings'],$parameters);
+
 @endphp
 
-<div 
-    id="{{$field['settings']['htmlId'] or ''}}" 
-    class="{{$field['settings']['htmlClass'] or ''}}"
->
-    <iframe  src="https://www.youtube.com/embed/{{$youtube_id}}?rel=0" frameborder="0" allowfullscreen width="100%" height="{{$field['settings']['height'] or '315'}}"></iframe>
-</div>
+@if ($visible)
+  <div 
+      id="{{$field['settings']['htmlId'] or ''}}" 
+      class="{{$field['settings']['htmlClass'] or ''}}"
+  >
+      <iframe  src="https://www.youtube.com/embed/{{$youtube_id}}?rel=0" frameborder="0" allowfullscreen width="100%" height="{{$field['settings']['height'] or '315'}}"></iframe>
+  </div>
+@endif
