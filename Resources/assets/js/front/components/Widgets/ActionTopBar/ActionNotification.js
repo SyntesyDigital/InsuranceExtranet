e9f8bc40a9e16d18f04ffe7e884d04835af6208e
Sergi Mínguez Regidor <sergiminguez@MacBook-Pro-de-Sergi.local>
2020-12-18 19:34:46 +0100

    modal modifications

--- Resources/assets/js/front/components/Widgets/ActionTopBar/ActionNotification.js
+++ Resources/assets/js/front/components/Widgets/ActionTopBar/ActionNotification.js
@@ -8,11 +8,86 @@ import './ActionTopBar.scss';
 export default class ActionNotification extends Component {
     constructor(props) {
         super(props)
+
+        const title = SITE_CONFIG_GENERAL.TITLE_NOTIFICATION_BTN !== undefined
+            && SITE_CONFIG_GENERAL.TITLE_NOTIFICATION_BTN.value !== null ?
+            SITE_CONFIG_GENERAL.TITLE_NOTIFICATION_BTN.value
+            : '';
+
+        const totalElement = SITE_CONFIG_GENERAL.WS_TOTAL_NOTIFICATION_BTN !== undefined
+            && SITE_CONFIG_GENERAL.WS_TOTAL_NOTIFICATION_BTN.value !== null ?
+            SITE_CONFIG_GENERAL.WS_TOTAL_NOTIFICATION_BTN.value
+            : '';
+
+        const tableElement = SITE_CONFIG_GENERAL.MODEL_EXECUTE_NOTIFICATION !== undefined
+            && SITE_CONFIG_GENERAL.MODEL_EXECUTE_NOTIFICATION.value !== null ?
+            SITE_CONFIG_GENERAL.MODEL_EXECUTE_NOTIFICATION.value
+            : '';
+        
+
         this.state = {
-            display: true,
-            isHover: false
+            display: false,
+            isHover: false,
+            title : title,
+            totalElement : totalElement,
+            tableElement : tableElement,
+            wsTotal : 0,
+            loadingElement : true,
+            elementModel : null,
+            elementObject : null
         }
         this.handleModalSidebar = this.handleModalSidebar.bind(this);
+    }
+
+    componentDidMount() {
+        this.query();
+        this.loadElement();
+    }
+
+    query() {
+
+        var self = this;
+        const { totalElement } = this.state;
+
+        axios.get('/architect/extranet/' + totalElement + '/model_values/data/1?')
+            .then(function (response) {
+                if (response.status == 200
+                    && response.data.modelValues !== undefined) {
+                    console.log("ActionNotification  :: componentDidMount => ", response.data);
+
+                    self.setState({
+                        wsTotal: response.data.modelValues !== undefined ? parseInt(response.data.modelValues[0].val1) : 0,
+                    });
+                } 
+
+            }).catch(function (error) {
+                console.log(error);
+                self.setState({
+                    loading: false
+                });
+            });
+    }
+
+    loadElement() {
+
+        var self = this;
+        axios.get(ASSETS+'/architect/extranet/element-modal/'+this.state.tableElement)
+            .then(function(response) {
+                if(response.status == 200
+                    && response.data !== undefined)
+                {
+                console.log("ModalTable :: data => ",response.data);
+
+                self.setState({
+                    elementModel : response.data.model,
+                    elementObject : response.data.element,
+                    loadingElement : false
+                });
+
+                }
+            }).catch(function (error) {
+                console.log(error);
+            });
     }
 
     // ==============================
@@ -23,13 +98,7 @@ export default class ActionNotification extends Component {
         this.setState({
             display: !this.state.display
         });
-        TweenMax.to(".modal-sidebar", 0.5, {
-            right: this.state.display ? "0px" : "-312px",
-            visibility: "visible"
-        }, {
-            ease: Power2.easeInOut,
-            onComplete: function () { }
-        });
+        
     }
 
     // ==============================
@@ -47,29 +116,28 @@ export default class ActionNotification extends Component {
     // ==============================
 
     render() {
-        const title = SITE_CONFIG_GENERAL.TITLE_NOTIFICATION_BTN !== undefined
-            && SITE_CONFIG_GENERAL.TITLE_NOTIFICATION_BTN.value !== null ?
-            SITE_CONFIG_GENERAL.TITLE_NOTIFICATION_BTN.value
-            : '';
-
-        const wsTotal = SITE_CONFIG_GENERAL.WS_TOTAL_NOTIFICATION_BTN !== undefined
-            && SITE_CONFIG_GENERAL.WS_TOTAL_NOTIFICATION_BTN.value !== null ?
-            SITE_CONFIG_GENERAL.WS_TOTAL_NOTIFICATION_BTN.value
-            : '';
-
+        
         return (
             <div>
-                <ModalSidebar />
+                {!this.state.loadingElement && 
+                    <ModalSidebar 
+                        display={this.state.display}
+                        elementObject={this.state.elementObject}
+                        elementModel={this.state.elementModel}
+                    />
+                }
                 <a
                     onClick={this.handleModalSidebar.bind(this)}
                     className="tooltip-link-action"
-                    title={title}
+                    title={this.state.title}
                     onMouseEnter={this.setIsHover.bind(this, true)}
                     onMouseLeave={this.setIsHover.bind(this, false)}
                 >
                     <span className="notification icon">
                         {this.state.isHover ? <MaskSvgNotificationHover /> : <MaskSvgNotification />}
-                        <span className="number">{wsTotal}</span>
+                        {this.state.wsTotal > 0 &&
+                            <span className="number">{this.state.wsTotal}</span>
+                        }
                     </span>
                 </a>
             </div>
--- Resources/assets/js/front/components/Widgets/ModalSidebar/ModalSidebar.js
+++ Resources/assets/js/front/components/Widgets/ModalSidebar/ModalSidebar.js
@@ -2,6 +2,17 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Modal from '../Modal';
 import './ModalSidebar.scss';
+import ListParser from '../Lists/ListParser';
+
+
+import {
+    parseNumber,
+    parseDate,
+    getConditionalFormating,
+    hasConditionalFormatting,
+    getConditionalIcon,
+    hasConditionalIcon
+} from '../functions';
 
 export default class ModalSidebar extends Component {
 
@@ -10,6 +21,21 @@ export default class ModalSidebar extends Component {
         this.state = {
             results: [1, 2, 3, 4, 5, 6, 7, 8, 9]
         }
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+
+        if(this.props.display != prevProps.display) {
+            TweenMax.to(".modal-sidebar", 0.5, {
+                right: this.props.display ? "0px" : "-312px",
+                visibility: "visible"
+            }, {
+                ease: Power2.easeInOut,
+                onComplete: function () { }
+            });
+
+        }
+
     }
 
     // ==============================
@@ -32,14 +58,82 @@ export default class ModalSidebar extends Component {
     // Renderers
     // ==============================
 
-    renderRowItems() {
-        return (this.state.results.map((item, i) => (
-            <div key={i} className="row-item">
+    renderField(item, identifier, field) {
+
+        var value = item[identifier];
+        var hasIcon = hasConditionalIcon(field, value);
+
+        if (field.type == "date") {
+            value = parseDate(value, field);
+        }
+        else if (field.type == "number") {
+            value = parseNumber(value, field, item, this.props.parameters);
+        }
+        else if (field.type == "text") {
+            switch (field.settings.format) {
+                case 'password':
+                    value = '******';
+                    break;
+            }
+        }
+
+        var style = getConditionalFormating(field, value);
+        var hasColor = hasConditionalFormatting(style);
+        var icon = getConditionalIcon(field, value);
+        var hasIcon = hasConditionalIcon(icon);
+
+
+        if (field.type == "file" || field.type == "file_ws_fusion") {
+            return <div dangerouslySetInnerHTML={{ __html: value }} />
+        }
+        // has route
+        else if (field.settings.hasRoute !== undefined && field.settings.hasRoute != null) {
+
+            return  <div className={(hasIcon ? 'has-icon' : '')}>
+                        <a href={item[identifier + "_url"]}>
+                            {hasIcon ? <i className={icon.icon}></i> : null} &nbsp;
+                            {item[identifier]}
+                        </a>
+                    </div>
+        }
+        // has default
+        else {
+            return  <div className={(hasIcon ? 'has-icon' : '')}>
+                        {hasIcon ? <i className={icon.icon}></i> : null}
+                        <div
+                            className={hasColor ? 'has-color' : ''}
+                            style={style}
+                            dangerouslySetInnerHTML={{
+                                __html: value
+                            }}
+                        />
+                    </div>
+        }
+    }
+
+    renderItem(item, elementObject) {
+    
+        var infos = [];
+
+        for (var key in elementObject.fields) {
+            // console.log("TypologyPaginated => ",items[key]);
+            var identifier = elementObject.fields[key].identifier
+            infos.push(
+                <div className="field-container" key={key}>
+                    {this.renderField(item, identifier, elementObject.fields[key])}
+                </div>
+            );
+        }
+
+        return (
+            <div className="row-item">
+                {infos}
+                {/*
                 <h5>{'Numero de contract'}</h5>
                 <a href="#" className="date-link" onClick={this.handleClickDate.bind(this)}><span className="date">22 avril 2020</span></a>
                 <a href="#" className="trash-link" onClick={this.handleRemoveItem.bind(this)}><span className="trash"></span></a>
+                */}
             </div>
-        ))
         );
     }
 
@@ -47,7 +141,21 @@ export default class ModalSidebar extends Component {
         return (
             <div className="modal-sidebar">
                 <div className="wrapper-items">
-                    {this.renderRowItems()}
+
+                    { this.props.display && 
+                        <ListParser
+                            customClass="notifications-list-container"
+                            field={null}
+                            elementObject={btoa(JSON.stringify(this.props.elementObject))}
+                            model={btoa(JSON.stringify(this.props.elementModel))}
+                            pagination={false}
+                            itemsPerPage={20}
+                            //columns={this.props.columns}
+                            //parameters={[]}
+                            renderItem={this.renderItem.bind(this)}
+                        />
+                    }
+
                 </div>
             </div>
         );
@@ -55,10 +163,8 @@ export default class ModalSidebar extends Component {
 }
 
 ModalSidebar.propTypes = {
-    title: PropTypes.string,
     display: PropTypes.bool,
-    zIndex: PropTypes.number,
-    onCancel: PropTypes.func,
-    onModalClose: PropTypes.func,
+    elementObject: PropTypes.object,
+    elementModel: PropTypes.object,
 };
 
