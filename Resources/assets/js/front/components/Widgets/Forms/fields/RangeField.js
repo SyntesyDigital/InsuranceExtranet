df3c649270ab50f678bcfc56ba4ea87f2c218ae9
Francisco Barceló <franciscobarcelo@Mac-mini-de-Francisco.local>
2021-01-08 13:16:52 +0100

    - Fix handler range

--- Resources/assets/js/front/components/Widgets/Forms/fields/RangeField.js
+++ Resources/assets/js/front/components/Widgets/Forms/fields/RangeField.js
@@ -1,40 +1,41 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import Slider from 'react-rangeslider'
-import 'react-rangeslider/lib/index.css'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Slider from 'react-rangeslider';
+import 'react-rangeslider/lib/index.css';
+import LabelTooltip from './../../../Common/LabelTooltip';
 
 export default class RangeField extends Component {
     constructor(props, context) {
         super(props, context)
-
         this.state = {
             active: '',
-            range: null
         }
     }
 
     handleChange(value) {
-        console.log("handleChange :: ", value)
-        // this.props.onChange(this.props.name, value);
-        this.setState({
-            range: value
-        })
-        if (this.state.range <= (this.props.minValue + 20)) {
+
+        this.props.onFieldChange({
+            name: this.props.field.identifier,
+            value: value
+        });
+
+        if (value <= (this.props.minValue + 20)) {
             this.setState({
                 active: 'hidden-label-right'
             });
-        } else if ((this.state.range >= (this.props.maxValue - 20))) {
+        } else if ((value >= (this.props.maxValue - 20))) {
             this.setState({
                 active: 'hidden-label-left'
             });
         } else {
             this.setState({
                 active: ''
-            })
+            });
         };
     }
 
     render() {
+        
         const rangeLabels = {
             0: this.props.minValue + '€',
             1000: this.props.maxValue + '€'
@@ -50,29 +51,53 @@ export default class RangeField extends Component {
         let isHidden = field.settings.hidden !== undefined && field.settings.hidden != null ?
             field.settings.hidden : false;
 
+        let hasDescription = field.settings.description !== undefined ?
+            field.settings.description : false;
+
         let isHideLabel = field.settings.hidelabel !== undefined ?
             field.settings.hidelabel : false;
 
+        let isLabelInline = field.settings.labelInline !== undefined ?
+            field.settings.labelInline : false;
+
+        var colClassLabel = isLabelInline ?
+            'field-container-col col-xs-5' :
+            'field-container-col col-xs-12';
+
+        var colClassInput = isLabelInline ?
+            'field-container-col col-xs-7' :
+            'field-container-col col-xs-12';
+
         return (
-            <div className={"range-field"}>
-                <div className={`row container-range ${this.state.active}`}>
-                    <div className="col-md-2 container-label">
-                        <p>
-                            {this.props.label}
-                            {isRequired &&
-                                <span className="required">&nbsp; *</span>
+            <div className={"form-group bmd-form-group " + (errors) + " " + (isHidden ? ' hidden' : '')}>
+                <div className={"range-field"}>
+                    <div className={`row field-container container-range ${this.state.active}`}>
+                        <div className={`container-label ${colClassLabel}`}>
+                            {!isHideLabel &&
+                                <label className={'bmd-label-floating'}>
+                                    {field.name}
+                                    {isRequired &&
+                                        <span className="required">&nbsp; *</span>
+                                    }
+                                    {hasDescription &&
+                                        <LabelTooltip
+                                            description={field.settings.description ?
+                                                field.settings.description : ''}
+                                        />
+                                    }
+                                </label>
                             }
-                        </p>
-                    </div>
-                    <div className="col-md-8">
-                        <Slider
-                            min={this.props.minValue}
-                            max={this.props.maxValue}
-                            value={this.props.value}
-                            labels={rangeLabels}
-                            handleLabel={this.props.value + '€'}
-                            onChange={this.handleChange.bind(this)}
-                        />
+                        </div>
+                        <div className={colClassInput}>
+                            <Slider
+                                min={this.props.minValue}
+                                max={this.props.maxValue}
+                                value={this.props.value}
+                                labels={rangeLabels}
+                                handleLabel={this.props.value + '€'}
+                                onChange={this.handleChange.bind(this)}
+                            />
+                        </div>
                     </div>
                 </div>
             </div>
